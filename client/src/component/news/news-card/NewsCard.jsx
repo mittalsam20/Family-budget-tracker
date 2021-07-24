1015e3b4937adfb592f9dbee78e1f65888621e40
Pradumna <pradumnabajoria@gmail.com>
2021-07-24 23:46:34 +0530

    added basic layout of news component

---
+++ client/src/component/news/news-card/NewsCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NewsCard.scss"
+
+function NewsCard() {
+    return (
+        <div className='news-card'>
+            Hii
+        </div>
+    )
+}
+
+export default NewsCard
--- client/src/pages/news/News.jsx
+++ client/src/pages/news/News.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./News.scss"
+import StockInNews from "../../component/news/stock-in-news/StockInNews";
+import NewsBoard from "../../component/news/news-board/NewsBoard";
 
 function News() {
-  return <div></div>;
+  return (
+    <div className='news'>
+      <div className="news-left">
+        <NewsBoard />
+      </div>
+      <div className="news-right">
+        <StockInNews />
+      </div>
+    </div>
+  );
 }
 
 export default News;
