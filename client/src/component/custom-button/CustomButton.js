2b5d3dd18d83c042f915c0dd2cd0e0900510650b
rohan_mittal <rohanmittal1366@gmail.com>
2021-07-25 00:34:39 +0530

    added list component in investment page

---
+++ client/src/component/custom-button/CustomButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./custombutton.scss";
+
+const CustomButton = ({ text }) => {
+  return <button className="custom-button">{text}</button>;
+};
+
+export default CustomButton;
